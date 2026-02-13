9433d4c0a9aaf0f15f4bf40ffee06ec5b73b6322
mohamadreza2780 <mohamadzeynali202020@gmail.com>
2026-02-13 22:25:22 +0330

    add index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const TelegramBot = require('node-telegram-bot-api');
+const { SocksProxyAgent } = require('socks-proxy-agent');
+
+// پروکسی SOCKS5
+const agent = new SocksProxyAgent('socks5://127.0.0.1:12334');
+
+const token = '8262761921:AAELxgpUXnxAa-_zaH9wJ9hqG7rWkropVt8';
+const bot = new TelegramBot(token, {
+    polling: true,
+    request: {
+        agent: agent
+    }
+});
+
+// start command
+bot.onText(/\/start/, (msg, match) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "hello");
+})
